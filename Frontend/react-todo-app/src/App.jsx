c8c88ffd825e4308bced5fe6da01ee334ce9153d
Ahmed Raed <ahmedraed101@yahoo.com>
2024-09-18 21:33:56 +0300

    adding login page

--- Frontend/react-todo-app/src/App.jsx
+++ Frontend/react-todo-app/src/App.jsx
@@ -1,7 +1,19 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Login from "./pages/Login/Login";
+import SignUp from "./pages/SignUp/SignUp";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Routes>
+        <Route path="/dashboard" exact element={<Home />} />
+        <Route path="/login" exact element={<Login />} />
+        <Route path="/signup" exact element={<SignUp />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
